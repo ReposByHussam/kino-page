dc7a21aaf0fbdf1d99fdd6dfa1e9404df4d1031d
ValentynaVoronkova <vella.dona@gmail.com>
2026-01-27 10:57:20 +0100

    more comments to routing.js

--- src/scripts/routing.js
+++ src/scripts/routing.js
@@ -1,9 +1,14 @@
+
+//function to turn static routes to dynamic routes (enables client-side navigation for elements with 'data-route' attributes)
 export function setupRouting() {
   document.addEventListener("click", (e) => {
+    //'e.target' - when the element is clicked '.closest()' searches up in DOM for the closest ancestor with attribute 'data-route' (e.g.button)
     const el = e.target.closest("[data-route]");
-    if (!el) return;
+    if (!el) return; //if 'data-route'=null -> no such elements found -> return (exit)
+    //'el.dataset' the element with all 'data-*' attributes 'dataset.route' in JS = 'data-route' in HTML (kebab-case)
     const route = el.dataset.route;
-    if (!route) return;
-    window.location.href = route;
+    if (!route) return; //if 'data-route' attribute is empty -> return (exit)
+    //'window.location' manages URL in browser, '.href' changes it to the specified route
+    window.location.href = route; 
   });
-}
+} 
