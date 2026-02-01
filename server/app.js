7c339eb2951c3bef9f6a4777fb2c90eb9f5c5fbf
ReposByHussam <developedbyhussam@gmail.com>
2026-02-01 18:54:58 +0100

    allowed for express to be able to read JSON

--- server/app.js
+++ server/app.js
@@ -3,106 +3,110 @@ import express from "express";
 import renderPage from "./renderPage.js";
 import apiScreenings from "./apiScreenings.js";
 
+//middleware to parse JSON bodies
+app.use(express.json());
+
 //with 'export default' variable can be imported to other file w/t {}: initApp instead of {initApp}
 export default function initApp(api) {
-    const app = express();
+  const app = express();
 
-    //1) ROUTE TO STATIC FILES
-    //route to static files .use('/folderName', express.static('./folderName')) should go before dynamic routes app.get(), otherwise it'll not be read
-    //'/src' - absolute route from the project root
-    //'./src' - relative route from the folderName
-    app.use('/src', express.static('./src'));
+  //1) ROUTE TO STATIC FILES
+  //route to static files .use('/folderName', express.static('./folderName')) should go before dynamic routes app.get(), otherwise it'll not be read
+  //'/src' - absolute route from the project root
+  //'./src' - relative route from the folderName
+  app.use("/src", express.static("./src"));
 
-    //2) DYNAMIC ROUTES: SSR - render layout and get it 
-    // SSR - start page
-    app.get('/', async (req, res) => {
-        try {
-            const movies = await api.loadMovies();
-            const htmlText = await renderPage('index', { movies }); //{movies} - object with data from API-JSON
-            res.send(htmlText);
-        } catch (error) {
-            console.error("Error loading movies:", error);
-            const htmlText = await renderPage('index', { movies: [] }); //if { movies } empty
-            res.send(htmlText);
-        }
-    });
+  //2) DYNAMIC ROUTES: SSR - render layout and get it
+  // SSR - start page
+  app.get("/", async (req, res) => {
+    try {
+      const movies = await api.loadMovies();
+      const htmlText = await renderPage("index", { movies }); //{movies} - object with data from API-JSON
+      res.send(htmlText);
+    } catch (error) {
+      console.error("Error loading movies:", error);
+      const htmlText = await renderPage("index", { movies: [] }); //if { movies } empty
+      res.send(htmlText);
+    }
+  });
 
-    // SSR all other pages
-    app.get('/about-us', async (req, res) => {
-        const htmlText = await renderPage('about-us');
-        res.send(htmlText);
-    });
+  // SSR all other pages
+  app.get("/about-us", async (req, res) => {
+    const htmlText = await renderPage("about-us");
+    res.send(htmlText);
+  });
 
-    app.get('/bistro', async (req, res) => {
-        const htmlText = await renderPage('bistro');
-        res.send(htmlText);
-    });
+  app.get("/bistro", async (req, res) => {
+    const htmlText = await renderPage("bistro");
+    res.send(htmlText);
+  });
 
-    app.get('/contact', async (req, res) => {
-        const htmlText = await renderPage('contact');
-        res.send(htmlText);
-    });
+  app.get("/contact", async (req, res) => {
+    const htmlText = await renderPage("contact");
+    res.send(htmlText);
+  });
 
-    app.get('/eventPage', async (req, res) => {
-        const htmlText = await renderPage('eventPage');
-        res.send(htmlText);
-    });
+  app.get("/eventPage", async (req, res) => {
+    const htmlText = await renderPage("eventPage");
+    res.send(htmlText);
+  });
 
-    // All movies (cards)
-    app.get("/movies", async (req, res) => {
-        try {
-            const movies = await api.loadMovies();
-            const htmlText = await renderPage("movies", { movies });
-            res.send(htmlText);
-            //for the list (not cards), but with content/movies-list.hbs instead of content/movies.hbs
-            //const htmlText = await renderPage("movies-list", { movies });
-        } catch (error) {
-            console.error("Error loading movies:", error);
-            res.status(500).send("Kunde inte ladda filmerna");  //status 500 = server error
-        }
-    });
+  // All movies (cards)
+  app.get("/movies", async (req, res) => {
+    try {
+      const movies = await api.loadMovies();
+      const htmlText = await renderPage("movies", { movies });
+      res.send(htmlText);
+      //for the list (not cards), but with content/movies-list.hbs instead of content/movies.hbs
+      //const htmlText = await renderPage("movies-list", { movies });
+    } catch (error) {
+      console.error("Error loading movies:", error);
+      res.status(500).send("Kunde inte ladda filmerna"); //status 500 = server error
+    }
+  });
 
-    //One movie page by Id
-    app.get("/movies/:movieId", async (req, res) => {
-        try {
-            const movie = await api.loadMovie(req.params.movieId);
-            const htmlText = await renderPage("movie", { movie });
-            res.send(htmlText);
-        } catch (error) {
-            console.error(`Error loading movie ${req.params.movieId}:`, error);
-            res.status(404).send("Film hittades inte");
-        }
-    });
+  //One movie page by Id
+  app.get("/movies/:movieId", async (req, res) => {
+    try {
+      const movie = await api.loadMovie(req.params.movieId);
+      const htmlText = await renderPage("movie", { movie });
+      res.send(htmlText);
+    } catch (error) {
+      console.error(`Error loading movie ${req.params.movieId}:`, error);
+      res.status(404).send("Film hittades inte");
+    }
+  });
 
-    //endpoint for upcomiming screenings on a movie page
-    app.get('/api/movies/:movieId/screenings', async (req, res) => {
-        try {
-            const movieId = req.params.movieId;
+  //endpoint for upcomiming screenings on a movie page
+  app.get("/api/movies/:movieId/screenings", async (req, res) => {
+    try {
+      const movieId = req.params.movieId;
 
-            // 1. all screenings for movie by Id
-            const screenings = await apiScreenings.loadScreeningsByMovieId(movieId);
+      // 1. all screenings for movie by Id
+      const screenings = await apiScreenings.loadScreeningsByMovieId(movieId);
 
-            // 2. filter to get only upcoming movies
-            const upcomingScreenings = apiScreenings.getUpcomingScreeningsMoviePage(screenings);
+      // 2. filter to get only upcoming movies
+      const upcomingScreenings =
+        apiScreenings.getUpcomingScreeningsMoviePage(screenings);
 
-            // 3.  JSON HTTP-respons 200=OK
-            res.status(200).json({
-                success: true,
-                data: upcomingScreenings
-            });
-        } catch (error) {
-            console.error("ERROR:", error);
-            res.status(500).json({
-                success: false,
-                error: "Kunde inte ladda visningar"
-            });
-        }
-    });
+      // 3.  JSON HTTP-respons 200=OK
+      res.status(200).json({
+        success: true,
+        data: upcomingScreenings,
+      });
+    } catch (error) {
+      console.error("ERROR:", error);
+      res.status(500).json({
+        success: false,
+        error: "Kunde inte ladda visningar",
+      });
+    }
+  });
 
-    //FALLBACK for STATUS 404 msg
-    app.use((req, res) => {
-        res.status(404).send('Page is not found');
-    });
+  //FALLBACK for STATUS 404 msg
+  app.use((req, res) => {
+    res.status(404).send("Page is not found");
+  });
 
-    return app;
+  return app;
 }
